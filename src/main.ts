457f1e03b8d1b13b3a3f9e335646a5b0f5e20f5d
chunleong <chunleong.dothouz@gmail.com>
2025-06-28 12:42:04 +0800

    first commit

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+// src/main.ts
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Post Management System Api')
+    .setDescription(
+      'This API enables users to manage blog content with secure authentication and clearly defined user roles.',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
